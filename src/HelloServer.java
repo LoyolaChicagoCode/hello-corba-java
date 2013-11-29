8c9a640c4f9febcaf7bc703b7fb0b454a5757cb6
George K. Thiruvathukal <thiruvathukal@gmail.com>
2013-11-28 20:25:03 -0600

    various

--- src/HelloServer.java
+++ src/HelloServer.java
@@ -9,67 +9,67 @@ import org.omg.PortableServer.POA;
 import java.util.Properties;
 
 class HelloImpl extends HelloPOA {
-  private ORB orb;
+    private ORB orb;
 
-  public void setORB(ORB orb_val) {
-    orb = orb_val; 
-  }
-    
-  // implement sayHello() method
-  public String sayHello() {
-    return "\nHello world !!\n";
-  }
-    
-  // implement shutdown() method
-  public void shutdown() {
-    orb.shutdown(false);
-  }
+    public void setORB(ORB orb_val) {
+        orb = orb_val;
+    }
+
+    // implement sayHello() method
+    public String sayHello() {
+        return "\nHello world !!\n";
+    }
+
+    // implement shutdown() method
+    public void shutdown() {
+        orb.shutdown(false);
+    }
 }
 
 
 public class HelloServer {
 
-  public static void main(String args[]) {
-    try{
-      // create and initialize the ORB
-      ORB orb = ORB.init(args, null);
+    public static void main(String args[]) {
+        try {
+            // create and initialize the ORB
+            ORB orb = ORB.init(args, null);
 
-      // get reference to rootpoa & activate the POAManager
-      POA rootpoa = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
-      rootpoa.the_POAManager().activate();
+            // get reference to rootpoa & activate the POAManager
+            POA rootpoa = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
+            rootpoa.the_POAManager().activate();
 
-      // create servant and register it with the ORB
-      HelloImpl helloImpl = new HelloImpl();
-      helloImpl.setORB(orb); 
+            // create servant and register it with the ORB
+            HelloImpl helloImpl = new HelloImpl();
+            helloImpl.setORB(orb);
 
-      // get object reference from the servant
-      org.omg.CORBA.Object ref = rootpoa.servant_to_reference(helloImpl);
-      Hello href = HelloHelper.narrow(ref);
-          
-      // get the root naming context
-      org.omg.CORBA.Object objRef =
-          orb.resolve_initial_references("NameService");
-      // Use NamingContextExt which is part of the Interoperable
-      // Naming Service (INS) specification.
-      NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
+            // get object reference from the servant
+            org.omg.CORBA.Object ref = rootpoa.servant_to_reference(helloImpl);
+            Hello href = HelloHelper.narrow(ref);
 
-      // bind the Object Reference in Naming
-      String name = "Hello";
-      NameComponent path[] = ncRef.to_name( name );
-      ncRef.rebind(path, href);
+            // get the root naming context
+            org.omg.CORBA.Object objRef =
+                orb.resolve_initial_references("NameService");
+            // Use NamingContextExt which is part of the Interoperable
+            // Naming Service (INS) specification.
+            NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
 
-      System.out.println("HelloServer ready and waiting ...");
+            // bind the Object Reference in Naming
+            String name = "Hello";
+            NameComponent path[] = ncRef.to_name( name );
+            ncRef.rebind(path, href);
 
-      // wait for invocations from clients
-      orb.run();
-    } 
-        
-      catch (Exception e) {
-        System.err.println("ERROR: " + e);
-        e.printStackTrace(System.out);
-      }
-          
-      System.out.println("HelloServer Exiting ...");
-        
-  }
+            System.out.println("HelloServer ready and waiting ...");
+
+            // wait for invocations from clients
+            orb.run();
+        }
+
+        catch (Exception e) {
+            System.err.println("ERROR: " + e);
+            e.printStackTrace(System.out);
+        }
+
+        System.out.println("HelloServer Exiting ...");
+
+    }
 }
